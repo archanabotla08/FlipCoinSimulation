ea108c4c2116eeccf9460e4c9ce4611e58d8c063
Nisha271 <bukkenisha@gmail.com>
2020-07-11 01:22:06 +0530

    New Branch UC-1 created and modified file to flipcoin for single time and calcuate the winner

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,1 +1,13 @@
-echo "New Repo Created We are in master"
+#!/bin/bash
+Heads=0
+Tails=0
+RandomCheck=$(( RANDOM%2 ))
+if [ $RandomCheck -eq 1 ]
+then
+        Heads=`expr $Heads + 1`
+        echo "Heads Won = $Heads"
+else
+        Tails=`expr $Tails + 1`
+        echo "Tails won = $Tails"
+fi
+
