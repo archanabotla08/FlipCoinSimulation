5e008f9f3c819150c14b61ecd30de037a97b4733
Nisha271 <bukkenisha@gmail.com>
2020-07-11 01:31:34 +0530

    New Branch UC-2 created and modified file to flipcoin for multiple times and calcuate the winner

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
+read -p "Enter the Number of Time Coin Flip: " number
 Heads=0
 Tails=0
-RandomCheck=$(( RANDOM%2 ))
-if [ $RandomCheck -eq 1 ]
+
+while [ $flips -le $number ]
+do
+        RandomCheck=$(( RANDOM%2 ))
+        if [ $RandomCheck -eq 1 ]
+        then
+                Heads=`expr $Heads + 1`
+        else
+                Tails=`expr $Tails + 1`
+        fi
+        flips=`expr $flips + 1`
+done
+
+if [ $Heads -gt $Tails ]
 then
-        Heads=`expr $Heads + 1`
         echo "Heads Won = $Heads"
+elif [ $Heads -eq $Tails ]
+then
+        echo "Heads and Tails Both Won = $Heads"
 else
-        Tails=`expr $Tails + 1`
         echo "Tails won = $Tails"
 fi
 
