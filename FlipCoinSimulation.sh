06d55589955ad13a699de062ee2dab84d982e81d
Nisha271 <bukkenisha@gmail.com>
2020-07-11 09:20:35 +0530

    Newly created UC-4 branch and modified file for getting winner and if tie than Heads-Tails >2

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+read -p "Enter the Number of Time Coin Flip: " number
 Heads=0
 Tails=0
 flips=0
-while [ $flips ]
+while [ $flips -le $number ]
 do
         RandomCheck=$(( RANDOM%2 ))
         if [ $RandomCheck -eq 1 ]
@@ -28,7 +29,25 @@ elif [ $Heads -eq $Tails ]
 then
         echo "Heads and Tails Both Won = $Heads"
 	echo "Heads and Tails Both Won ="$(( $Heads - $Tails ))
-
+	 while [ $flips ]
+        do
+                RandomCheck=$(( RANDOM%2 ))
+                if [ $RandomCheck -eq 1 ]
+                then
+                        Heads=`expr $Heads + 1`
+                else
+                        Tails=`expr $Tails + 1`
+                fi
+                flips=`expr $flips + 1`
+        if [ $(( $Heads - $Tails )) -ge 2 ]
+        then
+                Result_For_UC3_Condition=$(( $Heads - $Tails ))
+                echo $Result_For_UC3_Condition
+                break;
+        else
+                continue;
+        fi
+        done
 else
         echo "Tails won = $Tails"
 	echo "Tails Won by Heads="$(( $Tails - $Heads ))
